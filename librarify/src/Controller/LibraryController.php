50735d2697fc90318c8823fd6ea5a0f0d555e49b
JonnathanBC <jonnabcl56@gmail.com>
2022-08-20 18:09:53 -0500

    Connection with DB mysql + Entity Book

--- librarify/src/Controller/LibraryController.php
+++ librarify/src/Controller/LibraryController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
+use App\Repository\BookRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use PhpParser\Node\Stmt\Return_;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -11,30 +15,57 @@ use Symfony\Component\Routing\Annotation\Route;
 class LibraryController extends AbstractController
 {
     /**
-     * @Route("/library/list", name="library_list")
+     * @Route("/books", name="books_get")
      */
-    public function list(Request $request, LoggerInterface $logger)
+    public function list(BookRepository $bookRepository)
     {
-        $title = $request->get('title', 'Alegría');
-        $logger->info('List action called inside function');
+        $books = $bookRepository->findAll();
+        $booksAsArray = [];
+        foreach ($books as $book) {
+            $booksAsArray[] = [
+                'id' => $book->getId(),
+                'title' => $book->getTitle(),
+                'image' => $book->getImage()
+            ];
+        };
         $response = new JsonResponse();
+        $response->setData([
+            'success' => true,
+            'data' => $booksAsArray
+        ]);
+        return $response;
+    }
+
+    /**
+     * @Route("/book/create", name="create_book")
+     */
+    public function createBook(Request $request, EntityManagerInterface $em)
+    {
+        $book = new Book();
+        $response = new JsonResponse();
+        $title = $request->get('title', null);
+        if(empty($title)) {
+            $response->setData([
+                'success' => false,
+                'error' => 'Title cannot be empty',
+                'data' => null
+            ]);
+            return $response;
+        }
+        $book->setTitle($title);
+        $em->persist($book);
+        $em->flush();
         $response->setData([
             'success' => true,
             'data' => [
                 [
-                    'id' => 1,
-                    'title' => 'Hacia rutas salvajes'
-                ],
-                [
-                    'id' => 2,
-                    'title' => 'El nombre del viento'
-                ],
-                [
-                    'id' => 2,
-                    'title' => $title
+                    'id' => $book->getId(),
+                    'title' => $book->getTitle()
                 ]
             ]
         ]);
+    
+       
         return $response;
     }
 }
