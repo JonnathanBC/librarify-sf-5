ee5f3598e220f0abe569a89f294268729696b8f5
JonnathanBC <jonnabcl56@gmail.com>
2022-08-20 21:29:45 -0500

    Forms

--- librarify/src/Controller/Api/BooksController.php
+++ librarify/src/Controller/Api/BooksController.php
@@ -3,10 +3,12 @@
 namespace App\Controller\Api;
 
 use App\Entity\Book;
+use App\Form\Type\BookFormType;
 use App\Repository\BookRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation\Request;
 
 class BooksController extends AbstractFOSRestController
 {
@@ -23,12 +25,16 @@ class BooksController extends AbstractFOSRestController
      * @Rest\Post(path="/books")
      * @Rest\View(serializerGroups={"book"}, serializerEnableMaxDepthChecks=true)
      */
-    public function postActions(EntityManagerInterface $em)
+    public function postActions(Request $request, EntityManagerInterface $em)
     {
         $book = new Book();
-        $book->setTitle('Installing FOS Rest Bundle');
-        $em->persist($book);
-        $em->flush();
-        return $book;
+        $form = $this->createForm(BookFormType::class, $book);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($book);
+            $em->flush();
+            return $book;
+        }
+        return $form;
     }
 }
