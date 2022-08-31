51973e0184afd098168213ec6f3a064559e08bd7
JonnathanBC <jonnabcl56@gmail.com>
2022-08-30 22:17:04 -0500

    Category Create and Get + Services + FormProcessor

---
+++ librarify/src/Services/Category/CategoryFormProcessor.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Services\Category;
+
+use App\Entity\Category;
+use App\Form\Model\CategoryDto;
+use App\Form\Type\CategoryFormType;
+use App\Repository\CategoryRepository;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+class CategoryFormProcessor 
+{
+
+    private GetCategory $getCategory;
+    private FormFactoryInterface $formFactoryInterface;
+    private CategoryRepository $categoryRepository;
+
+    public function __construct(
+        GetCategory $getCategory,
+        FormFactoryInterface $formFactoryInterface,
+        CategoryRepository $categoryRepository
+    ) {
+        $this->getCategory = $getCategory;
+        $this->formFactoryInterface = $formFactoryInterface;
+        $this->categoryRepository = $categoryRepository;
+    }
+
+    public function __invoke(Request $request, ?string $categoryId = null): array
+    {
+        $category = null;
+        $categoryDto = null;
+
+        if ($categoryId === null) {
+            $categoryDto = new CategoryDto();
+        } else {
+            $category = ($this->getCategory)($categoryId);
+            $categoryDto = CategoryDto::createFromCategory($category);
+        }
+
+        // handleForm
+        $form = $this->formFactoryInterface->create(CategoryFormType::class, $categoryDto);
+        $form->handleRequest($request);
+        if (!$form->isSubmitted()) {
+            return [null, 'Form is not submitted'];
+        }
+        if(!$form->isValid()) {
+            return [null, $form];
+        }
+
+        // Creation Category
+        if ($category === null) {
+            $category = Category::create($categoryDto->name);
+        } else {
+            $category->update($categoryDto->name);
+        }
+
+        $this->categoryRepository->save($category);
+        return [$category, null];
+    }
+}
---
+++ librarify/src/Services/Category/CreateCategory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Services\Category;
+
+use App\Entity\Category;
+use App\Repository\CategoryRepository;
+
+class CreateCategory
+{
+    private CategoryRepository $categoryRepository;
+
+    public function __construct(CategoryRepository $categoryRepository) {
+        $this->categoryRepository = $categoryRepository;
+    }
+
+    public function __invoke(string $name): ?Category
+    {
+        $category = Category::create($name);
+        $this->categoryRepository->save($category);
+        return $category;
+    }
+}
